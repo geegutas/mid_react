1e42a355d6cee590e7a28007555c830160a0c020
Gemmarie <Gemgutas>
2022-10-16 17:09:34 +0800

    commit

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,17 @@ import {useEffect, useState} from 'react';
 
 const Home = () => {
     return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-secondary ">
         <div className="home text-center">
             
             <h1 className="text-center m-4 "> 
-            <img src="logo.jpg" alt="" width="90px" height="90px" position="center"/> Mater Dei College Information System </h1> 
+            <img src="logo.jpg" alt="" width="90px" height="90px" position="center"
+            
+            /> College Student (Study Loads) </h1> 
             
             
         </div>
+        </nav>
     );
 }
  
